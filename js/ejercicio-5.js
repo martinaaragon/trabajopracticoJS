fd50905aa00d95a376d54b36a0decd806a0a821a
Aragon Martina <martinaaragon6@gmail.com>
2023-10-25 19:06:54 -0300

    realice el ejercicio 5 y modifique el script

---
+++ js/ejercicio-5.js
@@ -0,0 +1,5 @@
+let numero1 =parseInt(prompt("Ingrese un primer número"));
+let numero2 =parseInt(prompt("Ingrese un segundo número"));
+let resultado =(numero1 + numero2)
+document.write("El resultado de la suma de estos números sería:" + resultado);
+console.log(numero1+numero2)
